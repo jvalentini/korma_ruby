8185f641de02b3ee9e98ee1a99ed1dfc65545812
Justin Valentini <justin.valentini@gmail.com>
2013-11-25 18:55:07 -0500

    Implements insert functionality at a basic level.

--- korma.rb
+++ korma.rb
@@ -22,11 +22,38 @@ module Korma
     column_sql.join(', ')
   end
 
+  def parse_column_options(column_options)
+    raise ArgumentError unless column_options.is_a? Array
+
+    if column_options.count == 1
+      column_options = column_options[0]
+    end
+
+    # TODO: Make this lazy. Use yield.
+    columns, values = [], []
+    column_options.each do |key, value|
+      columns << key
+      values << "\"#{value}\""
+    end
+
+    [columns.join(', '), values.join(', ')]
+  end
+
+  def insert(entity, *columns)
+    binds = []
+
+    column_sql, values_sql = parse_column_options(columns)
+    sql = "INSERT INTO #{entity.tablename} (#{column_sql}) VALUES (#{values_sql})"
+
+    [sql, binds]
+  end
+
   def update(entity, columns, *predicates)
     binds = []
 
     column_sql = column_sql(columns, binds)
     where = predicates.any? ? ' WHERE ' : ''
+
     predicate_sql = ''
     predicates.each do |pred|
       predicate_sql += column_sql(pred, binds)
@@ -45,5 +72,6 @@ users = Entity.new(:users, { first: 'VARCHAR2(50)' }, { last: 'VARCHAR2(50)' })
 
 p users
 p select(users)
+p insert(users, { first: 'Hello', last: 'World' })
 p update(users, { first: 'Justin', last: 'Valentini'})
 p update(users, { first: 'Justin', last: 'Valentini'}, { id: 1 })
